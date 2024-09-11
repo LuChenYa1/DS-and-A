94504450d6e87bff701421481869b7cfb45e4877
lu-chen-ya <1934600272@qq.com>
2024-09-11 22:03:28 +0800

    9.11

--- Data Structure and Algorithm/UniDirEmptyHeadList.c
+++ Data Structure and Algorithm/UniDirEmptyHeadList.c
@@ -12,12 +12,15 @@ typedef struct Node
     struct Node *pNext;
 }LLNode;
 
-void AddToHead(LLNode * stHead, int Data);//头添加
-void AddToEnd(LLNode * stHead, int Data);//尾添加
+void AddToHead(LLNode * stHead, int Data);//* 头添加
+void AddToEnd(LLNode * stHead, int Data);//* 尾添加
+void AddBehandData(LLNode * stHead, int PosData, int DesData);//* 在目标数据后面插入一个节点
 
-void FreeList(LLNode * stHead);//释放链表
+LLNode * FindNodeByData(LLNode * stHead, int Data);//* 通过数据查找节点
 
-void PrintList(LLNode stHead);//打印链表
+void FreeList(LLNode * stHead);//* 释放链表
+
+void PrintList(LLNode stHead);//* 打印链表
 
 int main(void)
 {
@@ -27,11 +30,17 @@ int main(void)
     AddToHead(&stHead, 1);
     AddToEnd(&stHead, 2);
     AddToHead(&stHead, 3);
+    AddBehandData(&stHead, 2, 4);
+
+    if(FindNodeByData(&stHead, 4) == NULL)
+        printf("没找到\n");
+    else
+        printf("找到了\n");
 
     PrintList(stHead);
     
     FreeList(&stHead);
-    PrintList(stHead);
+    
     return 0;
 }
 
@@ -76,6 +85,40 @@ void AddToEnd(LLNode * stHead, int Data)//尾添加
     }
     //计数器加一
     stHead->Data ++;
+}
+
+void AddBehandData(LLNode * stHead, int PosData, int DesData)
+{
+    if(stHead == NULL || stHead->Data < 0)
+        return;
+    LLNode * pPosData = FindNodeByData(stHead, PosData);
+    //创建节点
+    LLNode * pNewNode = (LLNode *)malloc(sizeof(LLNode));
+    if(pNewNode != NULL)
+    {
+        //节点成员赋值
+        pNewNode->Data = DesData;
+        pNewNode->pNext = NULL;
+        //链接
+        pNewNode->pNext = pPosData->pNext;
+        pPosData->pNext = pNewNode;
+    }
+    //计数器加一
+    stHead->Data ++;
+}
+
+LLNode * FindNodeByData(LLNode * stHead, int Data)
+{
+    if(stHead == NULL || stHead->Data < 0)
+        return NULL;
+    LLNode * pCurrent = stHead->pNext;//! 注意第一个节点是空头，只存放<节点数量>
+    while(pCurrent != NULL)
+    {
+        if(pCurrent->Data == Data)
+            return pCurrent;
+        pCurrent = pCurrent->pNext;
+    }
+    return NULL;
 }
 
 void FreeList(LLNode * stHead)//释放链表
