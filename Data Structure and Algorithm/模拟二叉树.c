7c965d4739a6754e171cad9110a1cb9653388d67
lu-chen-ya <1934600272@qq.com>
2024-09-26 22:32:58 +0800

    24.9.26  二叉树节点入栈与出栈

--- Data Structure and Algorithm/模拟二叉树.c
+++ Data Structure and Algorithm/模拟二叉树.c
@@ -4,15 +4,75 @@
 //! 二叉树结构：根节点、叶子节点、兄弟节点、前驱节点、后驱节点、左子树、右子树
 //! 前序遍历、中序遍历、后序遍历：根左右、左根右、左右根
 
-
-typedef struct Node
+//* 二叉树节点定义
+typedef struct TNode
 {
     int Data;
-    struct Node * pFather;
-    struct Node * pLeft;
-    struct Node * pRight;
+    struct TNode * pFather;
+    struct TNode * pLeft;
+    struct TNode * pRight;
 }TreeNode;
 
+//* 栈节点定义
+typedef struct SNode
+{
+    TreeNode * pTreeNode;//装着二叉树节点
+    struct SNode * pNext;
+    struct SNode * pPre;
+}StackNode;
+
+//* 栈的空头
+StackNode Head;
+//* 栈的栈顶指针（尾指针）
+StackNode * pStackTop = &Head;
+
+//* 二叉树节点入栈（尾添加）
+void Push(TreeNode * pTreeNode)
+{
+    //参数合法性检测
+    if(pTreeNode == NULL)
+        return;
+    //申请栈节点
+    StackNode * pNewStackNode = (StackNode *)malloc(sizeof(StackNode));
+    if(pNewStackNode == NULL)
+        return;
+    //栈节点成员赋值
+    pNewStackNode->pTreeNode = pTreeNode;
+    pNewStackNode->pPre = NULL;
+    pNewStackNode->pNext = NULL;
+    //链接
+    pStackTop->pNext = pNewStackNode;
+    pNewStackNode->pPre = pStackTop;
+    pStackTop = pNewStackNode;
+}
+
+//* 二叉树节点出栈（尾删除）
+TreeNode * Pop(void)
+{
+    //栈为空
+    if(pStackTop == &Head)
+        return NULL;
+    //记录要删除的栈顶节点
+    TreeNode * pTreeNode = pStackTop->pTreeNode;
+    //释放栈顶节点、修改栈顶指针
+    pStackTop = pStackTop->pPre;
+    free(pStackTop->pNext);
+    pStackTop->pNext = NULL;
+    //返回删除的二叉树节点
+    return pTreeNode;
+}
+
+//! 利用递归遍历二叉树时类似栈的入栈出栈过程，一旦开始执行某节点函数的第一句，该节点就算入栈，执行了最后一句（遍历右子树）该节点就出栈
+void PrintTreeList(TreeNode * TreeNode) 
+{
+    if(TreeNode != NULL)
+    {
+        //这三行代码的不同位置可以实现三种遍历方式
+        printf("%d ", TreeNode->Data);
+        PrintTreeList(TreeNode->pLeft);
+        PrintTreeList(TreeNode->pRight);
+    }
+}
 
 int main(void)
 {
@@ -52,5 +112,7 @@ int main(void)
     t9.pFather = &t6;
     t10.pFather = &t6;
     
+    PrintTreeList(&t1);
+
     return 0;
 }
